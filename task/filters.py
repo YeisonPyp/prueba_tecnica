293306f28df7152b14957e3039456b540e1d2473
YeisonPyp <yeisonfabian_10@hotmail.com>
2023-04-20 09:57:54 -0500

    Agregando filtros tareas

--- task/filters.py
+++ task/filters.py
@@ -4,10 +4,10 @@ from .models import Persona, Tarea
 
 class TareaFilter(filters.FilterSet):
 
+    fecha = filters.DateFilter(field_name='fechalimite')
     fecha_min = filters.DateFilter(field_name='fechalimite', lookup_expr='gte', label='fecha_min')
-
     fecha_max = filters.DateFilter(field_name='fechalimite', lookup_expr='lte', label='fecha_max')
-    fecha = filters.DateFromToRangeFilter(field_name='fechalimite')
+    
 
     class Meta:
         model = Tarea
